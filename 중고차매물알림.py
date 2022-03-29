35bb188d5f29bda26bb1f522a6baeae7adb16697
Jeon Joonyong <60745844+draidev@users.noreply.github.com>
2022-03-29 18:30:50 +0900

    chromedriver 자동설치 코드 추가

--- 중고차매물알림.py
+++ 중고차매물알림.py
@@ -1,2 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+cage = ['Cat', 'Dog', 'Tiger']
+
+file = open('cage.txt', 'r', encoding='utf-8')  # 읽기 모드
+cage = file.readlines()  #  여러줄 한번에 읽어서 리스트에 담기
+
+for c in cage:
+    print(c.strip()) # Cat
+
+file.close() 
