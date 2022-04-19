13d558e9ba4e2be8f3100b798f0913692ea8b3d5
Jeon Joonyong <60745844+draidev@users.noreply.github.com>
2022-04-19 10:18:53 +0900

    delete

--- 중고차매물알림.py
+++
@@ -1,9 +0,0 @@
-cage = ['Cat', 'Dog', 'Tiger']
-
-file = open('cage.txt', 'r', encoding='utf-8')  # 읽기 모드
-cage = file.readlines()  #  여러줄 한번에 읽어서 리스트에 담기
-
-for c in cage:
-    print(c.strip()) # Cat
-
-file.close() 
